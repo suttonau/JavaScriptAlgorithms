ccd15a16e81b8c12f853d9dcd0530bdb124c5070
suttonau <sutton_mjbc@icloud.com>
2019-02-13 21:25:19 -0500

    Fibonacci Sum v1

---
+++ Fibonacci/FibonacciSum/FibonacciSum.js
@@ -0,0 +1,24 @@
+//problem from ProjectEuler exercise
+//function will find all even fibonacci numbers and return the sum of all even numbers of the sequence.
+//test limit of 10,000
+
+console.time("sean");
+function fibonacciEvenSum(limit) {
+  let x = 1;
+  let y = 2;
+  let sum = 0;
+  let evenSum = 2;
+
+  while (x + y < limit) {
+    sum = x + y;
+    x = y;
+    y = sum;
+
+    if (sum % 2 === 0) {
+      evensum += sum;
+      console.log(evensum);
+    }
+  }
+}
+fibonacciEvenSum(10000);
+console.timeEnd("sean");
